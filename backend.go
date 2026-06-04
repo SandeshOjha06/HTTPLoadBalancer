828d3ec98cbb8b04f8cdc1233eb447399f879cc5
Sandesh <sandeshojha70@gmail.com>
2026-06-04 09:29:44 +0545

    define backend

---
+++ backend.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http/httputil"
+	"net/url"
+	"sync"
+)
+
+type Backend struct {
+url *url.URL
+alive bool
+mux sync.RWMutex
+}
+
+func(b *Backend) setAlive(alive bool){
+	// use Lock() because to lock writing to prevent race condn
+b.mux.Lock()
+b.alive = alive
+defer b.mux.Unlock()
+}
+
+
+func(b *Backend) isAlive() bool {
+	// to check isalive only neer to read, so using RLock
+b.mux.RLock()
+defer b.mux.RUnlock()
+return b.alive
+} 
