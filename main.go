eb22ecabd26a356bb6adf8b98f8b8c67c14aef32
Sandesh <sandeshojha70@gmail.com>
2026-06-05 11:30:29 +0545

    feat: add graceful shutdown with SIGINT/SIGTERM handling

--- main.go
+++ main.go
@@ -1,56 +1,72 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 )
 
-
 func main() {
-    port := flag.Int("port", 8000, "port to listen on")
-
+	port := flag.Int("port", 8000, "port to listen on")
 	backends := flag.String("backends", "", "list of backends")
 
 	flag.Parse()
 
 	if *backends == "" {
-	log.Fatal("--backends list is reqd")
+		log.Fatal("--backends list is reqd")
 	}
 
 	backendList := strings.Split(*backends, ",")
 
-    lb := &LoadBalancer{}
+	lb := &LoadBalancer{}
 
-    for _, serverStr := range backendList {
-        serverUrl, err := url.Parse(serverStr)
-        if err != nil {
-            log.Fatalf("Failed to parse URL %s: %v", serverStr, err)
-        }
-        backend := &Backend{
-            url:   serverUrl,
-            alive: true,
-        }
-        lb.backends = append(lb.backends, backend)
-    }
+	for _, serverStr := range backendList {
+		serverUrl, err := url.Parse(serverStr)
+		if err != nil {
+			log.Fatalf("Failed to parse URL %s: %v", serverStr, err)
+		}
+		backend := &Backend{
+			url:   serverUrl,
+			alive: true,
+		}
+		lb.backends = append(lb.backends, backend)
+	}
 
-    go lb.StartHealthCheck()
+	go lb.StartHealthCheck()
 
 	fmt.Printf("Load balancer started on http://localhost:%d\n", *port)
 
-    mux := http.NewServeMux()
-    mux.HandleFunc("/status", lb.statusHandler)
-    mux.Handle("/", lb)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/status", lb.statusHandler)
+	mux.Handle("/", lb)
 
-    server := http.Server{
+	server := http.Server{
 		Addr:    fmt.Sprintf(":%d", *port),
-        Handler: mux,
-    }
+		Handler: mux,
+	}
+    //  Start the server in a background goroutine so it doesn't block the rest of main()
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
 
-    if err := server.ListenAndServe(); err != nil {
-        log.Fatal(err)
-    }
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit // blocks unitil ctrl+c
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
 }
