dfcac19a5056263d9ebcb2c6dca3c96c864c088d
Sandesh <sandeshojha70@gmail.com>
2026-06-05 10:21:56 +0545

    refactor: add cmd line flags to accept backend servers

--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "net/url"
-    "fmt"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
+
 func main() {
-    servers := []string{
-        "http://localhost:8081",
-        "http://localhost:8082",
-        "http://localhost:8083",
-    }
+    port := flag.Int("port", 8000, "port to listen on")
+
+	backends := flag.String("backends", "", "list of backends")
+
+	flag.Parse()
+
+	if *backends == "" {
+	log.Fatal("--backends list is reqd")
+	}
+
+	backendList := strings.Split(*backends, ",")
 
     lb := &LoadBalancer{}
 
-    for _, serverStr := range servers {
+    for _, serverStr := range backendList {
         serverUrl, err := url.Parse(serverStr)
         if err != nil {
             log.Fatalf("Failed to parse URL %s: %v", serverStr, err)
@@ -30,14 +39,14 @@ func main() {
 
     go lb.StartHealthCheck()
 
-    fmt.Println("Load balancer started on :8000")
+	fmt.Printf("Load balancer started on http://localhost:%d\n", *port)
 
     mux := http.NewServeMux()
     mux.HandleFunc("/status", lb.statusHandler)
     mux.Handle("/", lb)
 
     server := http.Server{
-        Addr:    ":8000",
+		Addr:    fmt.Sprintf(":%d", *port),
         Handler: mux,
     }
 
